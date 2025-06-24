f2b9208ab55a1e178e1be33f85cbff87818732e3
Disu <tobias_corrales@hotmail.com>
2025-06-24 13:07:02 -0300

    zzz lualine recordign macro

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -1,9 +1,29 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-        require('lualine').setup({
-              options = { theme = 'everforest' }
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    local function macro_recording()
+      local reg = vim.fn.reg_recording()
+      if reg == '' then return '' end
+      return 'Recording @' .. reg
+    end
+
+    require('lualine').setup({
+      options = { theme = 'everforest' },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { macro_recording, 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    })
+
+    vim.api.nvim_create_autocmd({ 'RecordingEnter', 'RecordingLeave' }, {
+      callback = function()
+        require('lualine').refresh()
+      end,
     })
   end
 }
